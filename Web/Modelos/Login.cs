e3dba34b9d4067862fc6432e53abc1d25901bed6
Nahum Casco <nahumcasco@gmail.com>
2023-03-29 18:51:25 -0600

    Commit
    Proyecto vacio

---
+++ Web/Modelos/Login.cs
@@ -0,0 +1,18 @@
+﻿namespace Modelos
+{
+    public class Login
+    {
+        public string CodigoUsuario { get; set; }
+        public string Contraseña { get; set; }
+
+        public Login()
+        {
+        }
+
+        public Login(string codigoUsuario, string contraseña)
+        {
+            CodigoUsuario=codigoUsuario;
+            Contraseña=contraseña;
+        }
+    }
+}
